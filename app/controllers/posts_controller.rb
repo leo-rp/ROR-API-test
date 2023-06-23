d509e474439024a704c099da60664a8634ac1499
leo-rp <leorock1988@hotmail.com>
2023-06-23 12:12:24 -0500

    added : - rescue intertnal error , active recorr error - create and update actions - check required fields for create and update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
-    
+
+  rescue_from Exception do |e|
+    log.error "#{e.message}"
+    render json: { error: e.message}, status: :internal_error
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: { error: e.message}, status: :unprocessable_entity
+  end
+
     # GET /post
     def index
         @posts = Post.where(published: true)
@@ -11,4 +20,27 @@ class PostsController < ApplicationController
       @post = Post.find(params[:id]) 
       render json: @post, status: :ok
     end
+
+    #POST /posts
+    def create
+      @post = Post.create!(create_params)
+      render json: @post, status: :created
+    end
+
+    #PUT /posts/{id}
+    def update
+      @post = Post.find(params[:id])
+      @post.update!(update_params)
+      render json: @post, status: :ok
+    end
+
+    private
+    def create_params
+      params.require(:post).permit(:title, :content, :published, :user_id)
+    end
+
+    def update_params
+      params.require(:post).permit(:title, :content, :published)
+    end
+
 end
