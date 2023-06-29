c66dfd318a0b017465282cfc7325498ea7575bb8
leo-rp <leorock1988@hotmail.com>
2023-06-29 00:32:25 -0500

    -added cache for search service

--- app/services/posts_search_service.rb
+++ app/services/posts_search_service.rb
@@ -1,6 +1,10 @@
 class PostsSearchService
     def self.search(curr_posts, query)
         #curr_posts.where("title like '%#{query}%'")
-        curr_posts.where("title LIKE  ?", curr_posts.sanitize_sql_like(query) + "%")
+        posts_ids = Rails.cache.fetch("posts_search/#{query}", expires_in: 1.hours) do 
+            curr_posts.where("title LIKE  ?", curr_posts.sanitize_sql_like(query) + "%").map(&:id)
+        end
+
+        curr_posts.where(id: posts_ids)
     end
 end
