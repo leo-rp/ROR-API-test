8b16a037015f7e7be22c9fb9eee11ad6a1ca5ec4
leo-rp <leorock1988@hotmail.com>
2023-06-28 23:23:42 -0500

    seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times {
+    user = FactoryBot.create :user
+    #FactoryBot.create :post, user_id: user.id
+    FactoryBot.create_list(:post, 5, user_id: user.id)
+}
